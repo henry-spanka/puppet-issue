e0129b0b12e69bc694a04724eeac1ea0c5b49ce9
Henry Spanka <henry@myvirtualserver.de>
2015-12-08 22:17:41 +0100

    initial import

---
+++ manifests/init.pp
@@ -0,0 +1,42 @@
+# == Class: issue
+#
+# Wrapper class to handle issue files
+#
+# === Parameters
+#
+# [*source*]
+#   Source file to be used
+#
+# === Examples
+#
+#  class { 'issue':
+#    source => 'puppet://files/my_issue.txt'
+#  }
+#
+# === Authors
+#
+# Alejandro Ricoveri <alejandroricoveri@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2014 Alejandro Ricoveri.
+#
+
+class issue (
+  $source = undef
+) {
+  #
+  # Issue file
+  #
+  file {
+    [
+    '/etc/issue',
+    '/etc/issue.net'
+    ]:
+    ensure => 'file',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => $source
+  }
+}
